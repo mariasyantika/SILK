fc007615dbe33d4cdc7c8305b2571819a5405c25
mariasyantika <mariasyantikam29@gmail.com>
2024-03-22 23:21:20 +0700

    cobacoba

--- rawatjalan-app/resources/views/rawatjalan.blade.php
+++ rawatjalan-app/resources/views/rawatjalan.blade.php
@@ -8,6 +8,7 @@
     <title>Rawat Jalan</title>
 
     <!-- Fonts -->
+    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
